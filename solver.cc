51d04ed058423767b59317026b75a6c518fb6b55
asalcedo31 <adrisal31@gmail.com>
2018-02-16 17:27:00 -0500

    runs without observer

--- solver.cc
+++ solver.cc
@@ -1,23 +1,53 @@
 #include <boost/numeric/odeint.hpp>
+#include <math.h> 
 
+const double pi = 3.14159265358979323846264338327950288;
 typedef std::vector< double > state_type;
 
-// void ball_on_oscillator( const state_type &x , state_type &dxdt , const double /* t */ )
-// {
-//     dxdt[0] = x[1];
-//     dxdt[1] = -x[0] - gam*x[1];
-// }
+
+double z_table(double t){
+	double A = 0.25; 
+	double T = 2;
+	return(A*cos((2*pi*t)/T));
+}
+
+double f_table(double z,  double t){
+	int B = 10;; 
+	double C = 2.0;
+	return(B*exp(-C*(z-z_table(t))));
+}
+
+
+
+struct write_state
+{
+    void operator()( const state_type &x ) const
+    {
+        std::cout << x[0] << "\t" << x[1] << "\n";
+    }
+};
+
+
+//x is the initial value vector and dpdt is a vector of derivatices 
+void ball_on_oscillator( const state_type &x , state_type &dpdt , const double  t )
+{
+	double g = 10;
+	int m = 1;
+	double table_f = f_table(x[2],t);
+    dpdt[0] = x[3]; //dx/dt = u
+    dpdt[1] = x[4]; //dy/dt = v
+    dpdt[2] = x[5]; //dz/dt = w
+    dpdt[3] = 0; //du/dt = 0
+    dpdt[4] = 0; //dv/dt = 0
+    dpdt[5] = (table_f-g)/m; //dw/dt = (f_table-f_gravity)/m
+
+
+}
+
 
 
 int main(){
-	// define the provided constants to be shared among functions
-	int m = 1;
-	double A = 0.25;
-	double T = 2;
-	int g = 10;
-	int B = 10;
-	double C = 2.0;
-
+	
 	// create and populate  initial state vector
 	state_type x(6);
 	x[0] = 0.0; // value for x
@@ -27,7 +57,10 @@ int main(){
 	x[4] = 0.0; //value for v
 	x[5] = 0.0; //value for w
 
+	size_t steps = boost::numeric::odeint::integrate( ball_on_oscillator ,
+        x , 0.0 , 10.0 , 0.1 );
 
+	std::cout << steps << std::endl;
 	return 0;
 }
 
